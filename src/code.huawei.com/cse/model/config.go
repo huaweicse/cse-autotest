5b46af5907191ee81feb2c5ab0ae066e9127d2c6
lizihuai <136703713@qq.com>
2019-01-14 10:03:08 +0800

    delete project id and region

--- src/code.huawei.com/cse/model/config.go
+++ src/code.huawei.com/cse/model/config.go
@@ -5,8 +5,6 @@ type GlobalCfg struct {
 	AppID           string    `yaml:"appid"`
 	Name            string    `yaml:"name"`
 	Version         string    `yaml:"version"`
-	Region          string    `yaml:"region"`
-	ProjectId       string    `yaml:"projectId"`
 	Http            Protocol  `yaml:"http"`
 	ConfigCenterUrl string    `yaml:"configCenterUrl"`
 	Cse             CseStruct `yaml:"cse"`
@@ -19,14 +17,10 @@ type Protocol struct {
 
 // CredentialStruct aksk信息
 type CredentialStruct struct {
-	AccessKey        string        `yaml:"accessKey"`
-	SecretKey        string        `yaml:"secretKey"`
-	AkskCustomCipher string        `yaml:"akskCustomCipher"`
-	Project          ProjectStruct `yaml:"project"`
-}
-
-type ProjectStruct struct {
+	AccessKey        string `yaml:"accessKey"`
+	SecretKey        string `yaml:"secretKey"`
 	AkskCustomCipher string `yaml:"akskCustomCipher"`
+	Project          string `yaml:"project"`
 }
 
 //CseStruct 设置注册中心SC的地址，要开哪些传输协议， 调用链信息等
